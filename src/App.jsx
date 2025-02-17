1a2e3fcfd4ee6b242cb8f4a481e1ef11bda3cc03
omurbek <omurbek.melisbekov.kk@gmail.com>
2025-02-17 06:28:36 -0800

    i This is commit for adding timer and some improvments :wq

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState , useRef} from 'react'
+import { useEffect, useState, useRef } from 'react'
 import { nanoid } from 'nanoid'
 import Confetti from 'react-confetti'
 import Die from './Die'
@@ -8,23 +8,29 @@ import './App.css'
 
 
 function App() {
-  
-  
-    
-  const [dice, setDice] = useState(()=>generateNum())
+
+
+  const [dice, setDice] = useState(() => generateNum())
+  const [time, setTime] = useState(0)
   const buttonRef = useRef(null)
-console.log(buttonRef)
+  const timeRef = useRef(null)
+
+  function startGame() {
+    if (time === 0)
+      timeRef.current = setInterval(() => setTime(prevTime => prevTime + 1), 1000)
+  }
 
   function newGame() {
     setDice(generateNum())
+    setTime(0)
   }
 
   useEffect(() => {
     if (gameWon) {
-    buttonRef.current.focus()
+      buttonRef.current.focus()
+      clearInterval(timeRef.current)
     }
   }, [newGame])
-
 
   function generateNum() {
     return new Array(10)
@@ -33,30 +39,34 @@ console.log(buttonRef)
         value: Math.ceil(Math.random() * 6),
         isHeld: false,
         id: nanoid()
-    }))
+      }))
   }
-      const gameWon = dice.every(die => die.value === dice[0].value) && dice.every(die => die.isHeld)
+
+  const gameWon = dice.every(die => die.value === dice[0].value) && dice.every(die => die.isHeld)
 
   function rollDice() {
     setDice(prevDice => prevDice.map(die => die.isHeld ? die : { ...die, value: Math.ceil(Math.random() * 6) }))
-}
+  }
 
 
   function hold(id) {
-    setDice(prevDice => prevDice.map(die => die.id === id ? { ...die, isHeld: !die.isHeld } : die ))
+    setDice(prevDice => prevDice.map(die => die.id === id ? { ...die, isHeld: !die.isHeld } : die))
   }
+
+
 
   return (
     <>
       <main>
-      {gameWon && <Confetti/>}
-      <h1 className="title">Tenzies</h1>
-      <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
+        {gameWon && <Confetti />}
+        <h1 className="title">Tenzies</h1>
+        <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
+        <h2>{time} {time > 1 ? "seconds passed" : "second passed"} </h2>
+        <button onClick={startGame} className='timer'>Start Timer</button>
         <div className="btn-container">
           {dice.map((el) => <Die key={el.id} hold={() => hold(el.id)} isHeld={el.isHeld} dice={el.value} />)}
         </div>
         <button className='roll-btn' ref={buttonRef} onClick={!gameWon ? rollDice : newGame}>{gameWon ? "New Game" : "Roll"}</button>
-
       </main>
     </>
   )
--- src/Die.jsx
+++ src/Die.jsx
@@ -3,7 +3,7 @@ export default function Die(props) {
     const style = {background: props.isHeld ? "#59E391" : "white"}
     return (
         <div className="btns">
-            <button style={{ ...style}}
+            <button className="dice" style={{ ...style}}
                 onClick={props.hold}
             >{props.dice}</button>
         </div>
